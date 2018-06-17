228c8355e58c151cd81a915c36dbebc450f37274
Martin Arana <martin.arval.19@gmail.com>
2018-06-17 17:51:51 -0500

    MapSpawner

--- DH-Unity/Assets/Scripts/MapSpawner.cs
+++ DH-Unity/Assets/Scripts/MapSpawner.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapSpawner : MonoBehaviour {
-	public GameObject[] Objects;
+	[Header("Objects")]
+	public GameObject Lampara;
+	public GameObject Mesa;
+	public GameObject Pared;
+	public GameObject CajaF;
+	public GameObject Llave;
+	public GameObject Nota;
+	public GameObject Enchufe;
+	public GameObject Fosforos;
+	public GameObject Puerta;
 	void Start () {
 		
 	}
