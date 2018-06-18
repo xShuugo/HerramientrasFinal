7c7bf5b53ab02a7f4639de28489bdd0b56c2ceee
Martin Arana <martin.arval.19@gmail.com>
2018-06-17 20:52:56 -0500

    Unity Update

--- DH-Unity/Assets/Scripts/MapSpawner.cs
+++ DH-Unity/Assets/Scripts/MapSpawner.cs
@@ -1,21 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MapSpawner : MonoBehaviour {
-	[Header("Objects")]
-	public GameObject Lampara;
-	public GameObject Mesa;
-	public GameObject Pared;
-	public GameObject CajaF;
-	public GameObject Llave;
-	public GameObject Nota;
-	public GameObject Enchufe;
-	public GameObject Fosforos;
-	public GameObject Puerta;
-	void Start () {
-		
-	}
-	
-	
+public class MapSpawner : MonoBehaviour
+{
+    [Header("File Reading")]
+    public TextAsset LevelFile;
+
+    [Header("Objects")]
+    public GameObject Lampara;
+    public GameObject Mesa;
+    public GameObject Pared;
+    public GameObject CajaF;
+    public GameObject Llave;
+    public GameObject Nota;
+    public GameObject Enchufe;
+    public GameObject Fosforos;
+    public GameObject Puerta;
+    void Start()
+    {
+        ReadFile();
+    }
+
+    void ReadFile()
+    {
+        string[] arg = LevelFile.ToString().Split('/');
+
+        GameObject.Find("LevelTitle").GetComponent<Text>().text = arg[0];
+
+        string[] PPos = arg[1].Split('|');
+        GameObject.Find("Player").transform.position = new Vector3(
+            float.Parse(PPos[0]),float.Parse(PPos[1]),float.Parse(PPos[2]));
+
+        for (int a = 2; a < arg.Length - 1; a++)
+        {
+            Debug.Log(arg[a]);
+            string[] prop = arg[a].Split('|');
+            //Debug.Log(prop[0]);
+            switch (prop[0].Trim())
+            {
+                case "Mesa": SpawnMesa(prop); break;
+                default: Debug.Log("Tipo no declarado"); break;
+            }
+        }
+    }
+
+    void SpawnMesa(string[] p)
+    {
+        GameObject m = Instantiate(Mesa,
+		 new Vector3(float.Parse(p[2]), 0, float.Parse(p[3])),
+		 Quaternion.Euler(0, float.Parse(p[4]), 0));
+		m.name = p[1];
+    }
 }
---
+++ DH-Unity/Assets/Scripts/Player/MouseLocker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLocker : MonoBehaviour {
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        // Lock/Unlock
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            if (Input.GetButtonUp("Cancel"))
+            { Cursor.lockState = CursorLockMode.None; Debug.Log("yep"); }
+        }
+        else
+        {
+            if ((Input.GetButtonUp("Fire1") || Input.GetButtonUp("Cancel")))
+                Cursor.lockState = CursorLockMode.Locked;
+        }
+       
+
+        // Hide/Unhide
+        if (Cursor.lockState == CursorLockMode.Locked)
+            Cursor.visible = false;
+        else Cursor.visible = true;
+    }
+}
